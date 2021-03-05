5cab68e1b4a143bba37318bd463d97794b5ff029
gavinfigueruelo <figueruelogavin@gmail.com>
2021-03-05 10:07:54 -0500

    iterface fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
+const BASE_URL = 'https://api.github.com/users/gavinfigueruelo';
 
+const generateAside = (data) => {
+  const source = document.querySelector("#profile").innerHTML;
+  const template = Handlebars.compile(source);
+  // const context = data;
+  const html = template(data);
 
-////////////////MY REPO
-const API_KEY = '63b3383ef6b4811d3e1aaf52d3b5826e3a7cc76e';
-const BASE_URL ='https://api.github.com/users/gavinfigueruelo';
+  document.querySelector(".user").innerHTML = html;
+}
+
+fetch(`${BASE_URL}`)
+  .then(response => response.json())
+  .then(data => generateAside(data));
+
+// fetch the info for repositories
+const generateRep = (data) => {
+  const source = document.getElementById("repositories").innerHTML;
+  const template = Handlebars.compile(source);
+  const context = data;
+  const html = template(context);
+  document.querySelector(".repos").innerHTML = html;
+}
+fetch(`${BASE_URL}/repos`)
+  .then(response => response.json())
+  .then(data => generateRep({
+    rep: data
+  }));
+
+//fethcing the organizations and turning it into an array
+  const generateOrgs = (data) => {
+  const source = document.getElementById("orgs").innerHTML;
+  const template = Handlebars.compile(source);
+  const context = data;
+  const html = template(context);
+  document.querySelector(".orgs").innerHTML = html;
+}
+fetch(`${BASE_URL}/orgs`)
+  .then(response => response.json())
+  .then(data => generateOrgs({orgs:data}));
